486799ac97e0d4fea612d2a5e65e594012fa58b8
Mpho Theletsane <m.theletsane@lab.clicklearning.org>
2023-05-04 09:38:53 +0200

    added iwa4

---
+++ IWA4/challenge1/scripts.js
@@ -0,0 +1,38 @@
+const date = 2050
+const status = 'student'
+const count = 0
+
+if (date == 2050) {
+	console.log("January", 'New Years Day')
+	console.log("March", 'Human Rights Day')
+
+	let date = "April"
+	console.log(date, 'Family Day')
+	console.log(date, 'Freedom Day')
+	
+	//let date = count + 4
+
+	function value (student) {
+	console.log("June", 'Youth Day')
+	//let count = count + 1
+    }
+
+	console.log('August', 'Womens Day')
+	console.log('September', 'Heritage Day')
+
+	date = 'December'
+	console.log(date, 'Day of Reconciliation')
+	let status = count + 3
+
+	if (status == "parent") {
+	console.log(date, 'Christmas Day')
+	//let count = count + 1
+  }
+
+	console.log(date, 'Day of Goodwill')
+	//let count = count + 1
+}
+
+console.log('Your status is:'+ status)
+console.log('The year is:'+ date)
+console.log('The total holidays is:'+ count)
