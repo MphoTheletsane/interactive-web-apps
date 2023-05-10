e46c16b0acf302b4d930138ae0dae23658662f3e
Mpho Theletsane <m.theletsane@lab.clicklearning.org>
2023-05-10 21:15:47 +0200

    git initial

--- IWA5/challenge1.js
+++ IWA5/challenge1.js
@@ -1,34 +1,46 @@
-FREE_WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'
+const FREE_WARNING = 'Free shipping only applies to single customer orders'
+const BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
+const NONE_SELECTED = '0'
 
-if (location = RSA) { shipping === 400 && currency === 'R' }
+const location = 'RSA'
+let shipping = null
+let currency = '$'
+let customers = 1
 
-if location = NAM
-shipping = 600 
-else shipping = 800
+if (location === RSA) {
+    shipping = 400 ;
+    currency ='R' 
+}
 
-shoes = 300 * 1
-toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries 35 * 2
-pens = 5 * 'NONE_SELECTED' 
+if (location === NAM) {
+shipping = 600 }
+else {shipping = 800}
 
-shipping = null
-currency = $
+const shoes = 300 * 1
+const toys = 100 * 5
+const shirts = 150 * 'NONE_SELECTED'
+const batteries = 35 * 2
+const pens = 5 * 'NONE_SELECTED' 
 
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
+const total = shoes + batteries + pens + shirts + toys
+    if total >= 1000 && location === 'RSA' || location === 'NAM'
 	if (location = NAM && customers < 2) {
 			if (location = RSA)
 		    shipping = 0 || calcShipping
 		}
-	}
+	
+
+
+
+
+
+
+
+		
+
+if (shipping === 0 && customers !== 1) { 
+    console.log(FREE_WARNING) 
 }
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
+location === 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
-
-customers = '1'
-const location = 'RSA'
-currency = null
