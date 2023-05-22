69f839de3ce9cba9d22d998bed00266a96a45b64
Mpho Theletsane <m.theletsane@lab.clicklearning.org>
2023-05-22 09:41:48 +0200

    git initial

--- IWA7/challenge3/scripts.js
+++ IWA7/challenge3/scripts.js
@@ -1,18 +1,23 @@
 const leoName = 'Leo'
 const leoSurname = 'Musvaire     '
-const leoBalance = '9394'
+const leoBalance = '-9394'
 
 const sarahName = 'Sarah    '
 const sarahSurname = 'Kleinhans'
-const sarahBalance = '4582.21000111'
+const sarahBalance = '-4582.21000111'
 
 const divider = '----------------------------------'
 
 // Only change below this line
 
-const owed = parseFloat(leoBalance) + parseFloat(sarahBalance);
-const leo = `${leoName} ${leoSurname} Owed R${parseFloat(sarahBalance).toFixed(2)}`;
-const sarah = `${sarahName} ${sarahSurname} Owed R${parseFloat(sarahBalance).toFixed(2)}`;
-const total = "Total amount owed: R" + owed.toFixed(2);
-const result = "\n" + leo + "\n" + sarah + "\n" + divider + "\n" + divider + "\n  " + total.padStart(42);
-console.log(result);
+const sarahPositive = sarahBalance * -1
+const leoPositive = leoBalance * -1
+const owed = parseInt(sarahPositive + leoBalance)
+const leo = `${leoName} ${leoSurname} (Owed : R${leoPositive})`
+const sarah = `${sarahName}${sarahSurname} (Owed : R${sarahPositive})`
+const total = "Total amount owed: "
+const result = sarahPositive + leoPositive
+const finalResult = result.toFixed(2)
+
+
+console.log('\n','\n',leo, '\n',sarah,'\n','\n',divider,'\n',total,'R:',finalResult,'\n',divider,'\n','\n')
